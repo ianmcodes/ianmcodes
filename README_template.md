c2bf52189b796ca99c3b98fe7565918e91ca53de
Ian McCall <ian@ianmccall.codes>
2020-07-13 16:26:45 -0500

    Scripts to generate README

---
+++ README_template.md
@@ -0,0 +1,34 @@
+![Banner](https://www.ianmccall.codes/assets/images/banner.jpg)
+
+### Hi there 👋
+
+<a href="https://twitter.com/ianmccallcodes"><img src="https://img.shields.io/twitter/url?style=social&url=http%3A%2F%2Ftwitter.com%2Fianmccallcodes" alt="Twitter URL" /></a>
+<a href="https://www.linkedin.com/in/ianmccallcodes/"><img src="https://img.shields.io/badge/LinkedIn--_.svg?style=social&logo=linkedin" alt="LinkedIn URL" /></a>
+
+I'm Ian! Here are some things I've been working on:
+
+* 🎓 Learning more about WebAssembly
+  * I'm working on a comparison of some languages that compile to WebAssembly
+* 🧪 Experimenting with Web NFC https://www.ianmccall.codes/web-nfc-experiments/
+* 🎓 Learning Chinese https://www.duolingo.com/profile/IanMCodes
+
+Latest Blog posts:
+{{#for feed.entry end=5}}
+ * [{{title._text}}]({{link._attr.href}})
+{{/for}}
+
+
+<!--
+**ianmcodes/ianmcodes** is a ✨ _special_ ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+
+Here are some ideas to get you started:
+
+- 🔭 I’m currently working on ...
+- 🌱 I’m currently learning ...
+- 👯 I’m looking to collaborate on ...
+- 🤔 I’m looking for help with ...
+- 💬 Ask me about ...
+- 📫 How to reach me: ...
+- 😄 Pronouns: ...
+- ⚡ Fun fact: ...
+-->
---
+++ index.js
@@ -0,0 +1,61 @@
+const Handlebars = require('handlebars');
+const fetch = require('node-fetch');
+const xml2js = require('xml2js');
+const fs = require('fs').promises;
+const { program } = require('commander');
+
+program
+  .option('-t, --template <file>', 'path to template file')
+  .option('-f, --feed <URL>', 'URL of blog feed')
+  .option('-o, --output <file>', 'File to write output to')
+  .parse(process.argv);
+
+const parser = new xml2js.Parser({
+  attrkey: '_attr',
+  charkey: '_text',
+  explicitCharkey: true,
+  normalizeTags: true,
+  explicitArray: false
+});
+const waitFor = [];
+
+Handlebars.registerHelper('for', (context, options) => {
+  let start = options.hash.start || 0;
+  let end = options.hash.end || context.length;
+  start *= 1;
+  end *= 1;
+  let ret = '';
+  for(let i = start; i < end; i++) {
+    ret += options.fn(context[i]);
+  }
+  return ret;
+})
+
+// read in template
+waitFor.push(fs.readFile(program.template)
+.then((data) => {
+  return Handlebars.compile(data.toString());
+}));
+// read in feed
+waitFor.push(fetch(program.feed)
+.then((res) => {
+  return res.text();
+})
+.then((data) => {
+  // parse feed
+  return parser.parseStringPromise(data)
+})
+.then((result) => {
+  console.log(result.feed.entry[0]);
+  return result;
+}));
+// build README
+Promise.all(waitFor)
+.then(([template, feed]) => {
+  // write out README
+  const result = template(feed)
+  console.log(result);
+  if(program.output) {
+    return fs.writeFile(program.output, result);
+  }
+});
